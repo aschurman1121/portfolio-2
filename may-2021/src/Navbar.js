ae170789be224c9c249ce3886fc5c2c0b4576a26
Alexandra Schurman <alexandraschurman@gmail.com>
2022-05-23 15:07:37 -0700

    nav bar, some style

--- may-2021/src/Navbar.js
+++ may-2021/src/Navbar.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import {Router, Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className='navbar'>
     <ul className='navbar__ul' >
-        <li className='navbar__item'>About Me</li>
-        <li className='navbar__item'>Projects</li>
-        <li className='navbar__item'>Contact</li> 
-        <li className='navbar__item'>Resume</li>
+        <Router>
+            <li className='navbar__item'>
+                <Link to='/'>About Me </Link>
+                </li>
+            <li className='navbar__item'>
+                <Link to='/projects'>Projects </Link>
+                </li>
+            <li className='navbar__item'>
+                <Link to='/contact'> Contact
+                </Link>
+                </li> 
+            <li className='navbar__item'>
+                <Link to='/resume'>Resume </Link>
+                </li> 
+        </Router>
     </ul>
     </div>
   )
